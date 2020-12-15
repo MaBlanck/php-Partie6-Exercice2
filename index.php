c95a54994be0173da066d4dfa3186d561d9df018
Max <maxblanck@outlook.fr>
2020-12-15 09:55:34 +0100

    php Partie 6  Exercice 2

--- index.php
+++ index.php
@@ -9,12 +9,24 @@
 
 <body>
     <h1>Exercice 2 Partie 6</h1>
-    <?php
-    if (isset($_GET['age'])): ?>
-    <p><?= $_GET['age']?></p>
-    <?php else:?>
-    <p><?= 'Veuillez renseigner votre âge' ?></p>
-    <?php endif ?> 
+    <?php if (!isset($_GET['firstname'])) : ?>
+        <p> Votre prénom est manquant</p>
+    <?php endif ?>
+    <?php if (!isset($_GET['lastname'])) : ?>
+        <p> Votre nom est manquant</p>
+    <?php endif; ?>
+    <?php if (isset($_GET['firstname']) && isset($_GET['lastname'])) : ?>
+        <p> Bonjour <?= $_GET['firstname']; ?> <?= $_GET['lastname']; ?></p>
+    <?php endif; ?>
+    <?php if (!isset($_GET['age'])) : ?>
+        <p><?= 'Veuillez renseigner votre âge' ?></p>
+    <?php else : ?>
+        <p>Vous avez <?= $_GET['age'] ?> ans.</p>
+    <?php endif; ?>
+
+
+
+    <a href="index.php?lastname=Nemare&firstname=Jean">Cliquez</a>
 </body>
 
 </html>
